b22988c963e2b0a76008e054644a076baf53568b
Lívia V. Gomes <114452531+ViviSGG@users.noreply.github.com>
2023-09-11 00:18:43 -0300

    edit h1

--- script.js
+++ script.js
@@ -9,10 +9,12 @@ function lampLigar() {
   lampada.src = "./assets/on.png";
   bg.style.background = "gold";
   titulo.style.color = "black";
+  titulo.innerHTML = "Está claro!" // innerHTML - altera o texto, assim como o textContent
 }
 
 function lampDesligar() {
   lampada.src = "./assets/off.png";
   bg.style.background = "black";
   titulo.style.color = "white";
+  titulo.textContent = "Está escuro!" // textContent - altera o conteúdo do texto, é mais usual do que o innerHTML
 }
